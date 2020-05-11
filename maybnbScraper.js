2d3066a7ebb5663b72d50acd4b11db103bdf1e47
mogwai <feelirabs@gmail.com>
2020-05-12 00:02:55 +0100

    Hard give up. Really tough to meaningfully scrape Airbnb this way

--- maybnbScraper.js
+++ maybnbScraper.js
@@ -1,11 +1,13 @@
 const rp = require('request-promise');
+const $ = require('cheerio')
 
 const URL = `https://www.airbnb.com/s/Seychelles/homes?adults=0&children=0&infants=0&place_id=ChIJO62-cP0hXCIR99pJ06DQ0zI&refinement_paths%5B%5D=%2Fhomes&search_type=unknown`
 
 rp(URL)
 .then((data)=> {
     //happy path
-    console.log(data)
+    console.log($('div > a', data).length);
+    console.log($('div > a', data));
 })
 .catch((err)=>{
     //sad path
